a59c2117b9087ef2c1d7c61851e5ac4de11ea6f0
Dirkmal <kamsam403@gmail.com>
2020-04-13 23:12:54 +0100

    Refactored available beds

--- src/estimator.js
+++ src/estimator.js
@@ -13,7 +13,7 @@ function severeCases(infections) {
 }
 
 function availableBeds(totalBeds, numberOfCases) {
-  return numberOfCases - Math.trunc(totalBeds * 0.35);
+  return Math.trunc(totalBeds * 0.35) - numberOfCases;
 }
 
 function ICUCases(infections) {
